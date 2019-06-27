b448c7bbeebabae60717991f1d0b3dadfcc14728
kellyeryan <kellyeryan@gmail.com>
2019-06-27 17:13:07 -0400

    Finish Learn Hide-and-Seek Lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector(".target")
+}
+
+function deepestChild() {
+  return document.querySelector("#grand-node div div div div")
+}
+
+function increaseRankBy(n) {
+  var increaseRank = document.querySelectorAll(".ranked-list li")
+  for (var i = 0; i < increaseRank.length; i++) {
+    increaseRank[i].innerHTML = parseInt(increaseRank[i].innerHTML) + n
+  }
+}
